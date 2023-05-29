430341b7602a532cb28ac82841c48e0de468ace1
Nobbycode <norbertguda0@gmail.com>
2023-05-29 04:37:17 -0700

    redis client

--- utils/redis.js
+++ utils/redis.js
@@ -1,38 +1,37 @@
-import redis from 'redis'
-import { promisify } from 'util'
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
+    this.client = createClient();
     this.client.on('error', () => {
       console.log('Redis client could not connect to server');
     });
   }
 
-
-  /** Function to test connection */
+  /** checks whether redis server is still alive */
   isAlive() {
     return this.client.connected;
   }
 
-  /** function to return a value*/
-  async get(key) {
-    const redisGet = promisify(this.client.get).bind(this.client);
-    const value = await redisGet(key);
-    return value;
-  }
-
-  /** Asynchronous function to set a value in redis*/
+  /** set value in redis server */
   async set(key, value, duration) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
     this.client.expire(key, duration);
   }
 
-  /** Function to delete a key*/
+  /** fetch value from redis server */
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const valu = await redisGet(key);
+    return valu;
+  }
+
+  /** unsets key from redis server */
   async del(key) {
-    const redisDelete = promisify(this.client.del).bind(this.client);
-    await redisDelete(key);
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
   }
 }
 
